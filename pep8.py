bdaf66b4bca3057a99ef1ed4033e38f0e047d9bf
Jaykb123 <60962996+Jaykb123@users.noreply.github.com>
2020-03-14 11:45:29 +0530

    Update pep8.py

--- pep8.py
+++ pep8.py
@@ -10,9 +10,7 @@ def server_program():
     host = socket.gethostname()
     port = 5000        
     server_socket = socket.socket()      
-    
     server_socket.bind((host, port))       
-
     server_socket.listen(2)
     conn, address = server_socket.accept()     
     print("Connection from: " + str(address))
